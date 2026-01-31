44b4234f2e452d112f88c34ae90ce2e58c4e0cd7
KabanFriends <yuukun2004@protonmail.ch>
2026-01-31 16:47:40 +0900

    build: 📦 add maven publishing for api

--- api/build.gradle.kts
+++ api/build.gradle.kts
@@ -1,3 +1,30 @@
+plugins {
+    `maven-publish`
+}
+
+base {
+    archivesName.set("firepalace-api")
+}
+
 dependencies {
     compileOnly(libs.hytale)
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("mavenJava") {
+            from(components["java"])
+        }
+    }
+
+    repositories {
+        maven {
+            name = "IroriPoweredMaven"
+            url = uri("https://maven.irori.cc/repository/public/")
+            credentials {
+                username = project.findProperty("irori_maven_username")?.toString() ?: ""
+                password = project.findProperty("irori_maven_password")?.toString() ?: ""
+            }
+        }
+    }
 }
