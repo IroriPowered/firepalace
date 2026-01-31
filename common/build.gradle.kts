1abffe41ec174becc5bf84d2a6a0b3f426080c16
KabanFriends <yuukun2004@protonmail.ch>
2026-02-01 02:44:28 +0900

    build: 📦 relocate shadow packages to avoid conflict errors

--- common/build.gradle.kts
+++ common/build.gradle.kts
@@ -19,6 +19,16 @@ tasks {
         dependsOn(shadowJar)
     }
 
+    shadowJar {
+        relocate("org.apache", "cc.irori.firepalace.libs.org.apache")
+        relocate("org.json", "cc.irori.firepalace.libs.org.json")
+        relocate("org.slf4j", "cc.irori.firepalace.libs.org.slf4j")
+        relocate("com.google.errorprone", "cc.irori.firepalace.libs.com.google.errorprone")
+        relocate("com.google.gson", "cc.irori.firepalace.libs.com.google.gson")
+        relocate("redis.clients.authentication", "cc.irori.firepalace.libs.redis.clients.authentication")
+        relocate("redis.clients.jedis", "cc.irori.firepalace.libs.redis.clients.jedis")
+    }
+
     withType<Checkstyle>().configureEach {
         // Checkstyle throws parsing errors for this file
         exclude("**/DownstreamStatusPacket.java")
