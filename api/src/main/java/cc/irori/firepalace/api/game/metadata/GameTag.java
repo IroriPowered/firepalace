06ad54d777833b26c726e2e5aa72b8ece20afd54
KabanFriends <yuukun2004@protonmail.ch>
2026-01-31 17:18:03 +0900

    feat: ✨ add game tags

--- api/src/main/java/cc/irori/firepalace/api/game/metadata/GameTag.java
+++ api/src/main/java/cc/irori/firepalace/api/game/metadata/GameTag.java
@@ -4,7 +4,12 @@ public enum GameTag {
 
   // Height must be 24 pixels
   SOLO("solo", 80),
-  TWO_OR_MORE("two_or_more", 80),
+  TWO_OR_MORE("two_or_more", 64),
+  THREE_OR_MORE("three_or_more", 64),
+  FOUR_OR_MORE("four_or_more", 64),
+  PVP("pvp", 40),
+  ACTION("action", 80),
+  COMING_SOON("coming_soon", 69)
   ;
 
   private final String id;
